94070b25bfd31b383b2c0c274de6684563bc4f50
Daniel Logarta <danielogarta09@gmail.com>
2025-01-09 14:40:15 +0800

    Analytics db migration bugfix 4

--- database/migrations/2025_01_09_033502_create_lar_pagss_analytics_db.php
+++ database/migrations/2025_01_09_033502_create_lar_pagss_analytics_db.php
@@ -11,16 +11,12 @@ return new class extends Migration
     {
         Schema::create('user_activity_logs', function (Blueprint $table) {
             $table->id();
-            $table->unsignedInteger('user_id');
+            $table->integer('user_id');
             $table->string('activity_type');
             $table->text('activity_details');
             $table->string('ip_address');
             $table->string('user_agent');
             $table->timestamps();
-            $table->foreign('user_id')
-                ->references('id')
-                ->on(config('database.connections.user_admin.database') . '.users')
-                ->onDelete('cascade');
         });
     }
 
