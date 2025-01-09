841a3da1ef8a6c1f747a465869300769e1b27eec
Daniel Logarta <danielogarta09@gmail.com>
2025-01-09 13:44:56 +0800

    Database Creation Command (through artisan)

---
+++ app/Console/Commands/CreateDatabases.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
+
+class CreateDatabases extends Command
+{
+    protected $signature = 'db:create-db';
+
+    protected $description = 'Create multiple predefined databases';
+
+    public function handle(): void
+    {
+        $databases = [
+            'lar_pagss',
+            'lar_pagss_users',
+            'lar_pagss_cms',
+            'lar_pagss_analytics',
+        ];
+
+        foreach ($databases as $dbName) {
+            try {
+                DB::statement("CREATE DATABASE IF NOT EXISTS `$dbName`");
+                $this->info("Database '$dbName' created successfully!");
+            } catch (\Exception $e) {
+                $this->error("Error creating database '$dbName': " . $e->getMessage());
+            }
+        }
+    }
+}
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,6 +15,9 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->alias(['checkPage' => CheckPageAccess::class]);
         $middleware->trustProxies(at: '*');
     })
+    ->withCommands([
+        __DIR__.'/../app/Domain/Orders/Commands',
+    ])
     ->withExceptions(function (Exceptions $exceptions) {
         //
     })->create();
